b9d379230ac83c97205ffcc2dc8be8db83ca097d
Michael Berg <mberg12@bu.edu>
2023-06-29 18:17:53 -0400

    exercise 1 of LINQ

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<string> nameOfGames = new List<string>() { "Madden 25", "Super Smash Bros", "Cod", "Super Mario Bros" };
+
+            var lengthOfGameName = nameOfGames.OrderBy(x => x.Length);
+            foreach (var x in lengthOfGameName)
+            {
+                Console.WriteLine(x);
+            }
         }
     }
 }
